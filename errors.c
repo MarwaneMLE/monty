62bdde7eb2b9617bf00470526d07d98381834cbc
Marwane <khmarwane10@gmail.com>
2023-10-18 16:10:39 -0700

    C - Stacks, Queues - LIFO, FIFO

---
+++ errors.c
@@ -0,0 +1,118 @@
+#include "monty.h"
+
+/**
+ * error - Prints appropriate error messages based on their error code.
+ * @error_code: The error codes and their meanings are as follows:
+ * (1) => No file or more than one file provided by the user.
+ * (2) => The provided file cannot be opened or read.
+ * (3) => Invalid instruction found in the provided file.
+ * (4) => Memory allocation failure.
+ * (5) => Non-integer parameter given to the "push" instruction.
+ * (6) => Stack is empty when attempting "pint."
+ * (7) => Stack is empty when attempting "pop."
+ * (8) => Stack is too short for the requested operation.
+ */
+void error(int error_code, ...)
+{
+	va_list args;
+
+	va_start(args, error_code);
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n", va_arg(args, char *));
+			break;
+		case 3:
+		{
+			int l_num = va_arg(args, int);
+			char *op = va_arg(args, char *);
+
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			break;
+		}
+		case 4:
+			fprintf(stderr, "Error: malloc failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(args, int));
+			break;
+		default:
+			break;
+	}
+
+	va_end(args);
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * more_error - Manages specific errors based on error codes.
+ * @error_code: The following error codes are supported:
+ * (6) => Occurs when the stack is empty during a "pint" operation.
+ * (7) => Occurs when the stack is empty during a "pop" operation.
+ * (8) => Occurs when the stack is too short for the requested operation.
+ * (9) => Occurs when division by zero is attempted.
+ */
+void more_error(int error_code, ...)
+{
+	va_list args;
+
+	va_start(args, error_code);
+	switch (error_code)
+	{
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n", va_arg(args, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(args, int));
+			break;
+		case 8:
+		{
+			int l_num = va_arg(args, unsigned int);
+			char *op = va_arg(args, char *);
+
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		}
+		case 9:
+			fprintf(stderr, "L%d: division by zero\n", va_arg(args, unsigned int));
+			break;
+		default:
+			break;
+	}
+	va_end(args);
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * string_error - Manages errors with specific error codes.
+ * @error_code: The following error codes are handled:
+ * (10) => When the value inside a node falls outside the ASCII bounds.
+ * (11) => When the stack is empty.
+ */
+void string_error(int error_code, ...)
+{
+	va_list args;
+
+	va_start(args, error_code);
+	int l_num = va_arg(args, int);
+
+	switch (error_code)
+	{
+		case 10:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	va_end(args);
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
---
+++ main.c
@@ -0,0 +1,89 @@
+#include "monty.h"
+
+/*stack_t *head = NULL;*/
+
+/**
+ * main - Entry point for the Monty program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * Return: Always returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	/* Check for the correct number of command-line arguments */
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - Creates a new stack node.
+ * @n: The integer value to be stored in the node.
+ * Return: A pointer to the newly created node or NULL on failure.
+ */
+stack_t *create_node(int n)
+{
+	/* Allocate memory for a new stack node */
+	stack_t *node = malloc(sizeof(stack_t));
+
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees all nodes in the stack.
+ */
+void free_nodes(void)
+{
+	/* Check if the stack is empty */
+	if (head == NULL)
+		return;
+
+	/* Free all nodes in the stack */
+	stack_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+/**
+ * add_to_queue - Adds a new node to the end of the queue.
+ * @new_node: Pointer to the new node to be added.
+ * @ln: Line number of the opcode (unused).
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused)) unsigned int ln)
+{
+	/* Check for valid input */
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+
+	if (head == NULL)
+	{
+		/* If the queue is empty, set the new node as the head */
+		head = *new_node;
+		return;
+	}
+
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	/* Add the new node to the end of the queue */
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+}
