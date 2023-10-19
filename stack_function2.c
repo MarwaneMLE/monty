ebb6de287a613092e3a86f27ea74c65a64829bd7
Marwane <khmarwane10@gmail.com>
2023-10-19 10:05:58 -0700

    C - Stacks, Queues - LIFO, FIFO

--- stack_function2.c
+++ stack_function2.c
@@ -1,32 +1,34 @@
 #include "monty.h"
 
 /**
- * nop - Does nothing.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * nop - No operation. Does nothing.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Line number of the opcode (unused).
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
+	/* This function is a no-op (does nothing). */
+	(void)stack; /* Ignore unused parameters.*/
 	(void)line_number;
 }
 
-
 /**
  * swap_nodes - Swaps the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Line number of the opcode.
  */
 void swap_nodes(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
-
+	/* Swaps the top two elements of the stack. */
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "swap");
-	tmp = (*stack)->next;
+		more_error(8, line_number, "swap");
+
+	stack_t *tmp = (*stack)->next;
 	(*stack)->next = tmp->next;
+
 	if (tmp->next != NULL)
 		tmp->next->prev = *stack;
+
 	tmp->next = *stack;
 	(*stack)->prev = tmp;
 	tmp->prev = NULL;
@@ -35,63 +37,55 @@ void swap_nodes(stack_t **stack, unsigned int line_number)
 
 /**
  * add_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Line number of the opcode.
  */
 void add_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
-
+	/* Adds the top two elements of the stack. */
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "add");
-
-	(*stack) = (*stack)->next;
-	sum = (*stack)->n + (*stack)->prev->n;
+		more_error(8, line_number, "add");
+	*stack = (*stack)->next;
+	int sum = (*stack)->n + (*stack)->prev->n;
 	(*stack)->n = sum;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
 
-
 /**
- * sub_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * sub_nodes - Subtracts the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Line number of the opcode.
  */
 void sub_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	if (stack == NULL or * stack == NULL or ( * stack)->next == NULL)
+		more_error(8, line_number, "sub");
 
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	*stack = (*stack)->next;
+	int difference = (*stack)->n - (*stack)->prev->n;
 
-		more_err(8, line_number, "sub");
-
-
-	(*stack) = (*stack)->next;
-	sum = (*stack)->n - (*stack)->prev->n;
-	(*stack)->n = sum;
+	(*stack)->n = difference;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
 
-
 /**
- * div_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * div_nodes - Divides the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Line number of the opcode.
  */
 void div_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
-
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "div");
+		more_error(8, line_number, "div");
 
 	if ((*stack)->n == 0)
-		more_err(9, line_number);
-	(*stack) = (*stack)->next;
-	sum = (*stack)->n / (*stack)->prev->n;
-	(*stack)->n = sum;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
+		more_error(9, line_number);
+
+	 *stack = (*stack)->next;
+	 int result = (*stack)->n / (*stack)->prev->n;
+	 (*stack)->n = result;
+	 free((*stack)->prev);
+	 (*stack)->prev = NULL;
 }
---
+++ stack_str.c
@@ -0,0 +1,86 @@
+#include "monty.h"
+
+/**
+ * print_char - Prints the ASCII value as a character.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Integer representing the line number of the opcode.
+ */
+void print_char(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		string_error(11, line_number);
+
+	int ascii = (*stack)->n;
+
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_number);
+
+	printf("%c\n", ascii);
+}
+
+/**
+ * print_str - Prints a string composed of ASCII characters.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @ln: Integer representing the line number of the opcode (unused).
+ */
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	stack_t *tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		int ascii = tmp->n;
+
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
+
+/**
+ * rotl - Rotates the first node of the stack to the bottom.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @ln: Integer representing the line number of the opcode (unused).
+ */
+void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	stack_t *tmp = *stack;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
+
+/**
+ * rotr - Rotates the last node of the stack to the top.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @ln: Integer representing the line number of the opcode (unused).
+ */
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+	stack_t *tmp = *stack;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
